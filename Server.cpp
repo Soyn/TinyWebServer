301241f3fff5dc260c551f39e7fce759fe7e99ae
Soyn <w604408482@163.com>
2015-10-19 20:06:14 +0800

    complete

--- Server.cpp
+++ Server.cpp
@@ -6,6 +6,7 @@
 * This is a simple web server by c++.
 * Created by Soyn. 15/09/15.
 **/
+
 void Server :: error(const char* msg)
 {
     std :: perror(msg);
@@ -21,8 +22,7 @@ Server :: Server(int argc, char **argv)
 ///desconstructor
 Server:: ~Server()
 {
-     close(newsockfd);
-     close(sockfd);
+    close(sockfd);
 }
 
 void Server :: PortError()
@@ -50,19 +50,6 @@ void Server :: AcceptError()
     if( newsockfd < 0)
         error("ERROR on accept");
 }
-
-void Server :: ReadingError()
-{
-    if(n < 0)
-        error("ERROR reading from socket");
-}
-
-void Server :: WritingError()
-{
-    if( n < 0)
-        error("ERROR writing to socket");
-}
-
 void Server :: CreateSocket()
 {
     sockfd = socket(AF_INET, SOCK_STREAM, 0);    //create a socket
@@ -83,18 +70,17 @@ void Server :: SetServerAdd()
 void Server :: EstablishConnect()
 {
     clilen = sizeof(cli_addr);
-    newsockfd = accept( sockfd, ( sockaddr*) &cli_addr, &clilen);
     while(1)
     {
-        //establish the connection0
+        //establish the connection
         newsockfd = accept(sockfd, (sockaddr*) &cli_addr, &clilen);
         AcceptError();
 
         pid = fork();      //create a new process to handle this connection
         if(pid < 0)
-            error("Error on accept");
+            error("Error on fork");
         if(pid == 0)
-        {
+        {-
             close(sockfd);
             dostuff(newsockfd);
             exit(0);  // the process exits
@@ -103,37 +89,22 @@ void Server :: EstablishConnect()
             close(newsockfd);  // the parent closes the newsockfd
     }/*end of while*/
 }
-
-void Server:: ReadMessage()
-{
-    bzero(buffer,256);
-    n = read( newsockfd, buffer, 255);
-    ReadingError();
-    std :: cout << "Here is the message: " << buffer << '\n';
-}
-
-void Server :: WriteMessage()
-{
-    n = write(newsockfd, "I get your message", 18);
-    WritingError();
-}
-
 void Server :: Run()
 {
     PortError();
     CreateSocket();
     SetServerAdd();
     EstablishConnect();
-    ReadMessage();
-    WriteMessage();
 }
 
 ///
 /// <summary>Handles all communication once a connection has been established</summary>
 /// <para name = "sock">socket file description</para name>
 ///
-void Server :: dostuff(int sock)
+void Server :: dostuff(int& sock)
 {
+    int n;
+    char buffer[256];
 
     bzero(buffer,256);
     n = read(sock,buffer,255);
--- Server.h
+++ Server.h
@@ -1,9 +1,6 @@
 #ifndef SERVER_H_INCLUDED
 #define SERVER_H_INCLUDED
 
-#ifndef SERVER_CPP_H_INCLUDED
-#define SERVER_CPP_H_INCLUDED
-
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
@@ -16,38 +13,27 @@
 * The server.h
 * Created by Soyn.25/9/15.
 **/
+
 class Server{
 public:
-    Server(int argc, char *(argv[]));
+    Server(int argc, char **argv);
     ~Server();
     void PortError();
     void OpenSockError();
     void BindError();
     void AcceptError();
-    void ReadingError();
-    void WritingError();
     void CreateSocket();
     void SetServerAdd();
     void EstablishConnect();
-    void ReadMessage();
-    void WriteMessage();
     void Run();
     void error(const char*);
-    void dostuff(int sock);
+    void dostuff(int&);
 private:
-    int sockfd, newsockfd, portno, argc,pid,n;
+    int sockfd, newsockfd, portno, argc,pid;
     socklen_t clilen;
-    char **argv, buffer[256];
+    char **argv;
     sockaddr_in serv_addr, cli_addr;
 };
-
-
-#endif // SERVER_CPP_H_INCLUDED
-
-
-
-
-
 
 
 #endif // SERVER_H_INCLUDED
