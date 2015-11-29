514f87123abeb1e72d6b02bb962bec44ad24505d
Soyn <w604408482@163.com>
2015-11-29 13:55:15 +0800

    clear the extra codes

--- Server.h
+++ Server.h
@@ -8,12 +8,20 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <arpa/inet.h>
+#define VERSION 1.0
+#define ERROR 42
+#define LOG 42
+#define FORBIDDEN 403
+#define NOTFOUND 404
+
 
 /**
 * The server.h
 * Created by Soyn.25/9/15.
 **/
-
 class Server{
 public:
     Server(int argc, char **argv);
@@ -28,8 +36,9 @@ public:
     void Run();
     void error(const char*);
     void dostuff(int&);
+
 private:
-    int sockfd, newsockfd, portno, argc,pid;
+    int sockfd, newsockfd, portno, argc,pid,port,hit;
     socklen_t clilen;
     char **argv;
     sockaddr_in serv_addr, cli_addr;
